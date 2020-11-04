5ad4364d04d2c6b238c96207257737a6caa628a9
Alexandra Țihonschi <70908434+alexandratihonschi@users.noreply.github.com>
2020-11-04 20:59:39 +0200

    Add files via upload

---
+++ pr.3.py
@@ -0,0 +1,4 @@
+n=int(input("n este egal cu "))
+for i in range (1, n):
+    if i%2==0:
+        print('nr pare sunt:', i)
---
+++ pr.4.py
@@ -0,0 +1,5 @@
+a=int(input("a este egal cu "))
+b=int(input("b este egal cu "))
+for i in range (a, b):
+    if i%2!=0:
+        print('nr impare sunt:', i)
